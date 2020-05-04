d605c6b9e1964ec3835f659659be5063e8b8ad50
kedziorno <kjoblak85@gmail.com>
2020-05-04 22:34:06 +0200

    add cgroup file

---
+++ g42/stream_descriptor/build/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cgcreate -g memory:grp1
+echo 200M > /sys/fs/cgroup/memory/grp1/memory.limit_in_bytes
+echo 1G > /sys/fs/cgroup/memory/grp1/memory.memsw.limit_in_bytes
+make
+# server
+#cgexec -g memory:grp1 sudo ./stream_descriptor -s -u
+# client
+#cgexec -g memory:grp1 sudo ./stream_descriptor -c 10.137.0.15 -u
