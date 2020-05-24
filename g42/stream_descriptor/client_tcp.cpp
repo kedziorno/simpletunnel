15c6dbd690f6b9343bbc4c1398641e11eff5d57f
kedziorno <kjoblak85@gmail.com>
2020-05-24 19:39:54 +0200

    client_tcp - async_handshake - client

--- g42/stream_descriptor/client_tcp.cpp
+++ g42/stream_descriptor/client_tcp.cpp
@@ -49,64 +49,67 @@ client_tcp::client_tcp(boost::asio::io_context &io_context, boost::asio::ssl::co
 
 void client_tcp::run()
 {
-	m_socket_tcp_ssl.handshake(boost::asio::ssl::stream_base::client, m_error_code);
-	if (m_error_code) {
-		pfp_fact("Handshake : failed , " << m_error_code.message());
-	} else {
-		pfp_fact("Handshake : OK");
-		size_t loop_idx = 0;
-		while (1) {
-			m_io_context.get()->reset();
-			pfp_fact("Loop [" << ++loop_idx << "] : (tun_in/tun_out/socket_in/socket_out) -> (" << m_statistics.tun_in << "/" << m_statistics.tun_out << "/" << m_statistics.socket_in << "/" << m_statistics.socket_out << ")");
+//	m_socket_tcp_ssl.handshake(boost::asio::ssl::stream_base::client, m_error_code);
+	m_socket_tcp_ssl.async_handshake(boost::asio::ssl::stream_base::client, [&](const boost::system::error_code & error_code){
+		if (m_error_code) {
+			pfp_fact("Handshake : failed , " << m_error_code.message());
+		} else {
+			pfp_fact("Handshake : OK");
+			size_t loop_idx = 0;
+			while (1) {
+				m_io_context.get()->reset();
+				pfp_fact("Loop [" << ++loop_idx << "] : (tun_in/tun_out/socket_in/socket_out) -> (" << m_statistics.tun_in << "/" << m_statistics.tun_out << "/" << m_statistics.socket_in << "/" << m_statistics.socket_out << ")");
 
-			std::array<unsigned char, BUFFER_SIZE> request1;
-			request1.fill(0);
-			boost::asio::mutable_buffer mb1 = boost::asio::buffer(request1, BUFFER_SIZE);
-			m_socket_tcp_ssl.async_read_some(mb1, [&] (const boost::system::error_code& error, std::size_t bytes_transferred) {
-				if (!error) {
-					pfp_fact("Read " << bytes_transferred << " bytes from socket");
-					pfp_fact("dump : " << n_pfp::dbgstr_hex(mb1.data(), bytes_transferred));
-					m_statistics.socket_in += bytes_transferred;
-					boost::asio::mutable_buffer mb = boost::asio::buffer(mb1.data(), bytes_transferred);
-					boost::system::error_code ec;
-					size_t sd_ws = m_stream_descriptor.get()->write_some(mb, ec);
-					if (!ec) {
-						pfp_fact("Write " << sd_ws << " bytes to fd=" << m_stream_descriptor.get()->native_handle());
-						pfp_fact("dump : " << n_pfp::dbgstr_hex(mb.data(), sd_ws));
-						m_statistics.tun_out += sd_ws;
+				std::array<unsigned char, BUFFER_SIZE> request1;
+				request1.fill(0);
+				boost::asio::mutable_buffer mb1 = boost::asio::buffer(request1, BUFFER_SIZE);
+				m_socket_tcp_ssl.async_read_some(mb1, [&] (const boost::system::error_code& error, std::size_t bytes_transferred) {
+					if (!error) {
+						pfp_fact("Read " << bytes_transferred << " bytes from socket");
+						pfp_fact("dump : " << n_pfp::dbgstr_hex(mb1.data(), bytes_transferred));
+						m_statistics.socket_in += bytes_transferred;
+						boost::asio::mutable_buffer mb = boost::asio::buffer(mb1.data(), bytes_transferred);
+						boost::system::error_code ec;
+						size_t sd_ws = m_stream_descriptor.get()->write_some(mb, ec);
+						if (!ec) {
+							pfp_fact("Write " << sd_ws << " bytes to fd=" << m_stream_descriptor.get()->native_handle());
+							pfp_fact("dump : " << n_pfp::dbgstr_hex(mb.data(), sd_ws));
+							m_statistics.tun_out += sd_ws;
+						} else {
+							pfp_fact("Error on write to " << TUN0 << " : " << ec.message());
+						}
 					} else {
-						pfp_fact("Error on write to " << TUN0 << " : " << ec.message());
+						pfp_fact("Error on read from socket : " << error.message());
 					}
-				} else {
-					pfp_fact("Error on read from socket : " << error.message());
-				}
-			});
+				});
 
-			std::array<unsigned char, BUFFER_SIZE> request2;
-			request2.fill(0);
-			boost::asio::mutable_buffer mb2 = boost::asio::buffer(request2, BUFFER_SIZE);
-			m_stream_descriptor.get()->async_read_some(mb2, [&] (const boost::system::error_code& error, std::size_t bytes_transferred) {
-				if (!error) {
-					pfp_fact("Read " << bytes_transferred << " bytes from fd=" << m_stream_descriptor.get()->native_handle());
-					pfp_fact("dump : " << n_pfp::dbgstr_hex(mb2.data(), bytes_transferred));
-					m_statistics.tun_in += bytes_transferred;
-					boost::asio::mutable_buffer mb = boost::asio::buffer(mb2.data(), bytes_transferred);
-					boost::system::error_code ec;
-					size_t ws2 = m_socket_tcp_ssl.write_some(mb, ec);
-					if (!ec) {
-						pfp_fact("Write " << ws2 << " bytes to socket");
-						pfp_fact("dump : " << n_pfp::dbgstr_hex(mb.data(), ws2));
-						m_statistics.socket_out += ws2;
+				std::array<unsigned char, BUFFER_SIZE> request2;
+				request2.fill(0);
+				boost::asio::mutable_buffer mb2 = boost::asio::buffer(request2, BUFFER_SIZE);
+				m_stream_descriptor.get()->async_read_some(mb2, [&] (const boost::system::error_code& error, std::size_t bytes_transferred) {
+					if (!error) {
+						pfp_fact("Read " << bytes_transferred << " bytes from fd=" << m_stream_descriptor.get()->native_handle());
+						pfp_fact("dump : " << n_pfp::dbgstr_hex(mb2.data(), bytes_transferred));
+						m_statistics.tun_in += bytes_transferred;
+						boost::asio::mutable_buffer mb = boost::asio::buffer(mb2.data(), bytes_transferred);
+						boost::system::error_code ec;
+						size_t ws2 = m_socket_tcp_ssl.write_some(mb, ec);
+						if (!ec) {
+							pfp_fact("Write " << ws2 << " bytes to socket");
+							pfp_fact("dump : " << n_pfp::dbgstr_hex(mb.data(), ws2));
+							m_statistics.socket_out += ws2;
+						} else {
+							pfp_fact("Error on write to socket : " << ec.message());
+						}
 					} else {
-						pfp_fact("Error on write to socket : " << ec.message());
+						pfp_fact("Error on read from " << TUN0 << " : " << error.message());
 					}
-				} else {
-					pfp_fact("Error on read from " << TUN0 << " : " << error.message());
-				}
-			});
+				});
 
-			m_io_context.get()->run();
-			m_io_context.get()->restart();
+				m_io_context.get()->run();
+				m_io_context.get()->restart();
+			}
 		}
-	}
+	});
+	m_io_context.get()->run();
 }
